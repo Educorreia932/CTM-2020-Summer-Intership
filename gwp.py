e14aa4519e9315c396d0db2be5285a876f9e255f
Educorreia932 <skelozard1@gmail.com>
2020-07-04 17:19:03 +0100

    Reading FAP data from input file and organized everything into functions

--- gwp.py
+++ gwp.py
@@ -1,14 +1,38 @@
+import csv
 from math import *
+from sympy.abc import x
+from sympy import Poly
+from sympy.solvers.inequalities import solve_rational_inequalities
 
-# class FAP:
+FAPS= []
+
+class FAP:
+    def __init__(self, line):
+        self.id = line[0]
+        self.x = line[1]
+        self.y = line[2]
+        self.z = line[3]
+        self.traffic = line[4]
     
+#  Open input file with the FAPs data
+with open("input.csv", "r") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
     
-
-K = -20 * log10((4 * pi) / (3 * 10 ^ 8)) - 20 * log10(5250 * 10 ^ 6) - (-85) 
-
-#  Transmission Power (dBm)
-PT = 0
-
+    line_count = 0
+    
+    for line in csv_reader:
+        if line_count != 0:
+            fap = FAP(line)
+        
+            FAPS.append(fap)
+                       
+        line_count += 1
+        
+def solve_equation():
+    K = -20 * log10((4 * pi) / (3 * 10 ^ 8)) - 20 * log10(5250 * 10 ^ 6) - (-85) 
+    
+    return 
+                
 # (𝑥𝑈𝐴𝑉 𝑟𝑒𝑙𝑎𝑦 −𝑥𝐹𝐴𝑃 𝑁) ^ 2
 #  + (𝑦𝑈𝐴𝑉 𝑟𝑒𝑙𝑎𝑦 −𝑦𝐹𝐴𝑃 𝑁) ^ 2
 #  + (𝑧𝑈𝐴𝑉 𝑟𝑒𝑙𝑎𝑦 −𝑧𝐹𝐴𝑃 𝑁) ^ 2
@@ -17,11 +41,17 @@ PT = 0
 # 20
 # 2
 
-# while true:
-#     Solve
+def gwp():
+    #  Transmission Power (dBm)
+    PT = 0
     
-#     if (𝑥, 𝑦, 𝑧)𝑈𝐴𝑉 𝑟𝑒𝑙𝑎𝑦 != None:
-#         return 𝑃𝑇, (𝑥, 𝑦, 𝑧)𝑟𝑒𝑙𝑎�
-    
-#     else:
-#         PT += 1
+    while True:
+        solution = solve_equation()
+        
+        if solution != None:
+            return PT, solution
+        
+        else:
+            PT += 1
+            
+print(gwp())
